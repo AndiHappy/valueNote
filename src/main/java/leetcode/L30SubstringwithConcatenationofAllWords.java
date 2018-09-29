c54a9f9d0365cbd3940972656cc674ea47f8869b
翟刘振 <zhailz@chanjet.com>
2018-09-29 16:23:43 +0800

    L28~L29

---
+++ src/main/java/leetcode/L30SubstringwithConcatenationofAllWords.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zhailz
+ *
+ * @version 2018年9月29日 下午4:00:20
+ */
+public class L30SubstringwithConcatenationofAllWords {
+	
+	/*
+
+
+	You are given a string, s, and a list of words, words, that are all of the same length.
+	Find all starting indices of substring(s) in s that is a concatenation of each word in words exactly once and without any intervening characters.
+
+	Example 1:
+
+	Input:
+	  s = "barfoothefoobarman",
+	  words = ["foo","bar"]
+	Output: [0,9]
+	Explanation: Substrings starting at index 0 and 9 are "barfoor" and "foobar" respectively.
+	The output order does not matter, returning [9,0] is fine too.
+	Example 2:
+
+	Input:
+	  s = "wordgoodstudentgoodword",
+	  words = ["word","student"]
+	Output: []
+*/
+	
+	 public List<Integer> findSubstring(String s, String[] words) {
+		 List<Integer> res = new ArrayList<Integer>();
+		 return res;
+	    }
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
