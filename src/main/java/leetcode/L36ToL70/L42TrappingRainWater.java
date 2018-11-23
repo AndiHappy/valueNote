ff3d2da92f73b67bf240cd8170e0479b3e426fbb
翟刘振 <zhailiuzhen@cmcm.com>
2018-11-23 10:21:51 +0800

    L41

---
+++ src/main/java/leetcode/L36ToL70/L42TrappingRainWater.java
@@ -0,0 +1,34 @@
+package leetcode.L36ToL70;
+
+/**
+ * @author zhailz
+ * 2018年11月22日 下午10:08:56
+ */
+public class L42TrappingRainWater {
+
+	/**
+	
+	Given n non-negative integers representing an elevation map where 
+	the width of each bar is 1, compute how much water it is able to trap after raining.
+	
+	The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. 
+	In this case, 6 units of rain water (blue section) are being trapped.
+	Thanks Marcos for contributing this image!
+	
+	Example:
+	
+	Input: [0,1,0,2,1,0,1,3,2,1,2,1]
+	Output: 6
+	 * 
+	 * */
+
+	public int trap(int[] height) {
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
